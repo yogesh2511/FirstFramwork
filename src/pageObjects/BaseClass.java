7359f0222bac841007d97f2ff18903a1fee23f22
yogeshsolanki <yogeshsolanki@RIPLBRD49.rangamindia.com>
2017-06-16 00:40:19 -0400

    Second commit

--- src/pageObjects/BaseClass.java
+++ src/pageObjects/BaseClass.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 public class BaseClass {
 	public static WebDriver driver;
 	public static boolean bResult;
-	public  BaseClass(WebDriver driver){
+	public  BaseClass(WebDriver driver)
+	{
+		//this is the second commit!!
 		BaseClass.driver = driver;
 		BaseClass.bResult = true;
 	}
